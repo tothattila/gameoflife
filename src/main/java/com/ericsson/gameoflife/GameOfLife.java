3ace4c6a0f42e028c5a6d21f16e2d44c54bb7da4
eatttth <attila.toth@ericsson.com>
2012-10-31 13:03:57 +0100

    ADD: main class and maven project

---
+++ src/main/java/com/ericsson/gameoflife/GameOfLife.java
@@ -0,0 +1,16 @@
+package com.ericsson.gameoflife;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eatttth
+ * Date: 10/30/12
+ * Time: 12:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameOfLife {
+
+    public static void main(final String params[]) {
+        System.out.println("Hello Agileday World!");
+    }
+
+}
