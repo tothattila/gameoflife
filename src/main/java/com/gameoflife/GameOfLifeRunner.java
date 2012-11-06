da7a5c8363a0a98e3148dc4c238371b17882f9be
eatttth <attila.toth@ericsson.com>
2012-11-06 13:42:06 +0100

    ADD: primitive runner class

---
+++ src/main/java/com/gameoflife/GameOfLifeRunner.java
@@ -0,0 +1,41 @@
+package com.gameoflife;
+
+import com.gameoflife.util.BoardFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eatttth
+ * Date: 10/30/12
+ * Time: 12:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameOfLifeRunner {
+
+    private static void usage() {
+        System.out.println("Usage: command <stepcount> <board>");
+        System.out.println("  For example command 15 1,1,0;1,1,0;0,0,0 will do 15 steps on still board");
+        System.out.println("        1 1 0");
+        System.out.println("        1 1 0");
+        System.out.println("        0 0 0");
+    }
+
+    public static void main(final String params[]) {
+        int stepCount;
+        String board;
+        try {
+           stepCount = Integer.valueOf(params[0]);
+           board = params[1];
+           BoardFactory factory = new BoardFactory();
+           GameOfLife gameOfLife = new GameOfLife(new Engine(factory.parse(board)),'O');
+           gameOfLife.step(stepCount);
+           gameOfLife.printBoard();
+        } catch(NumberFormatException exc)  {
+           usage();
+        } catch (ArrayIndexOutOfBoundsException exc) {
+           usage();
+        }
+
+
+    }
+
+}
